d061ad83ca79305bfbac99d8dab7efd6be54a5c9
Christopher Howard <choward20@hawkmail.hccfl.edu>
2015-02-05 19:24:23 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Area51{
+
+  public static main void(Strings[]args){
+    System.out.println("In the begining I created this world,/n ");
+    System.out.println("and in the end I will destroy it!");
+  }
+
+}
